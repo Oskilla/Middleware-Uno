1f4fb8bbe9b3d489de1a8e075daeae2c1520017e
damien <damiepere@gmail.com>
2020-09-29 13:15:30 +0200

    Basics class add

---
+++ Uno/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	
+	static final String[] Figures = {"Numéro","Interdit","Sens","+2","+4","Couleur"};
+	
+	static final String[] Couleurs = {"Rouge","Vert","Jaune","Bleu","Noir"};
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
