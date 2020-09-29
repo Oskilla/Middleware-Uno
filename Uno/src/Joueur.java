1bc475fa4ea0498e621cd3368835514fd51927c2
Oskilla <questionaoskilla@hotmail.fr>
2020-09-29 11:12:57 +0200

    modification en plus

--- Uno/src/Joueur.java
+++ Uno/src/Joueur.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Joueur {
 	private String identifiant;
@@ -11,6 +12,7 @@ public class Joueur {
 	 * procédure mainInit appelé 1 fois en début de jeu pour distribuer les 7 cartes de la main initial du joueur.
 	 */
 	public void mainInit(JeuDeCarte jeu) {
+		main = new ArrayList<Carte>();
 		for(int i = 1; i<= 7; i++) {
 			Carte tmp;
 			tmp = jeu.tireCarte(); // return une Carte au hasard présent dans le paquet, la carte tiré doit être retiré du paquet.
@@ -21,7 +23,14 @@ public class Joueur {
 	 * procédure appelé quand le joueur joue une carte. Le joueur doit jouer une carte de sa main
 	 */
 	public void joueCarte(Carte carte) {
-		
+		//peut etre faire un try catch ici
+		if(main.contains(carte)) {
+			main.remove(carte);
+			//joue la carte lol
+		}
+		else {
+			
+		}
 	}
 	/*
 	 * procédure quand le joueur ne peut pas jouer de carte et doit en pioché une.
