797436257634076d50231c8891f3b2aefee05d68
Oskilla <questionaoskilla@hotmail.fr>
2020-10-06 09:10:11 +0200

    quelque modification encore en plus suivant le diagramme de classe

--- Uno/src/Uno.java
+++ Uno/src/Uno.java
@@ -23,34 +23,34 @@ public class Uno {
     }
   }
   public Uno(List<String> joueurs) {
+	    
 	  InitGame(joueurs);
   }
 
   public void InitGame(List<String> joueurs){
-	couleurs.add("Rouge");
-	couleurs.add("Bleu");
-	couleurs.add("Jaune");
-	couleurs.add("Vert");
-    for (int i=0;i<couleurs.size() ; i++) {
-    	for(int j=0; j<=9; j++) {
-    		CarteNumero crt = new CarteNumero(couleurs.get(i),"",j);
-    		pioche.add(crt);
-    	}
-    	CarteAction crta1 = new CarteAction(couleurs.get(i),"","Joker");
-    	CarteAction crta2 = new CarteAction(couleurs.get(i),"","+4");
-    	CarteAction crta3 = new CarteAction(couleurs.get(i),"","+2");
-    	CarteAction crta4 = new CarteAction(couleurs.get(i),"","+2");
-    	CarteAction crta5 = new CarteAction(couleurs.get(i),"","Change Sens");
-    	CarteAction crta6 = new CarteAction(couleurs.get(i),"","Change Sens");
-    	pioche.add(crta1);
-    	pioche.add(crta2);
-    	pioche.add(crta3);
-    	pioche.add(crta4);
-    	pioche.add(crta5);
-    	pioche.add(crta6);
-    }
-    this.melangerList(this.pioche);
-    
+		couleurs.add("Rouge");
+		couleurs.add("Bleu");
+		couleurs.add("Jaune");
+		couleurs.add("Vert");
+	    for (int i=0;i<couleurs.size() ; i++) {
+	    	for(int j=0; j<=9; j++) {
+	    		CarteNumero crt = new CarteNumero(couleurs.get(i),"carte "+couleurs.get(i)+" numéro "+j,j);
+	    		pioche.add(crt);
+	    	}
+	    	CarteAction crta1 = new CarteAction(couleurs.get(i),"Carte joker","Joker");
+	    	CarteAction crta2 = new CarteAction(couleurs.get(i),"Carte +4","+4"); 
+	    	CarteAction crta3 = new CarteAction(couleurs.get(i),"Carte +2 "+couleurs.get(i),"+2");
+	    	CarteAction crta4 = new CarteAction(couleurs.get(i),"Carte +2 "+couleurs.get(i),"+2");
+	    	CarteAction crta5 = new CarteAction(couleurs.get(i),"Carte changement sens "+couleurs.get(i),"Change Sens");
+	    	CarteAction crta6 = new CarteAction(couleurs.get(i),"Carte changement sens "+couleurs.get(i),"Change Sens");
+	    	pioche.add(crta1);
+	    	pioche.add(crta2);
+	    	pioche.add(crta3);
+	    	pioche.add(crta4);
+	    	pioche.add(crta5);
+	    	pioche.add(crta6);
+	    }
+	    this.melangerList(this.pioche);
     for(int i = 1; i<=joueurs.size();i++) {
     	Joueur jo = new Joueur(joueurs.get(i),this);
     	this.joueurs.add(jo);
@@ -67,5 +67,26 @@ public class Uno {
   }
   public List<Carte> getPioche(){
 	  return pioche;
+  }
+  public List<Carte> getTalon(){
+	  return talon;
+  }
+  public List<Joueur> getJoueurs(){
+	  return joueurs;
+  }
+  public Joueur getJoueur(int i) {
+	  return joueurs.get(i);
+  }
+  public Joueur getCourant() {
+	  return courant;
+  }
+  public void setCourant(Joueur newCourant) {
+	  courant = newCourant;
+  }
+  public boolean isGameOver() {
+	  return GameOver;
+  }
+  public int getSens() {
+	  return sens;
   }
 }
